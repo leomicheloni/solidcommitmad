6c5e11ba537f7cf12ebd3a06ee0729c76004f995
Leonardo Micheloni <leomicheloni@hotmail.com>
2019-11-22 10:38:42 +0100

    tests fixed

--- spec/invoice.spec.ts
+++ spec/invoice.spec.ts
@@ -3,18 +3,16 @@ import { Recipe } from "../src/recipe";
 import { Ingredient } from "../src/ingredient";
 
 
-describe("invoice behavior", function(){
-    it("should consider shipping cost in total", function(){
-        var a = new Invoice();
-        a.shipping = 7;
+describe("invoice behavior", function () {
+    it("should consider shipping cost in total", function () {
         let recipe = new Recipe();
-        let ingredient =new Ingredient();
+        var a = new Invoice(recipe, 7);
+        let ingredient = new Ingredient();
         ingredient.price = 7;
         ingredient.items = 3;
-        recipe.addIngredient( ingredient);
-        a.recipe = recipe;
+        ingredient.selected = true;
+        recipe.addIngredient(ingredient);
 
-
-        expect(a.total()).toBe(28);
+        expect(a.total).toBe(28);
     })
 });
